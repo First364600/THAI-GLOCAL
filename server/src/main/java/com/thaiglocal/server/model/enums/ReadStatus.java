6527cf8a959b0df9ad22083e994ce96a7b1dfee0
Ekkasit-Ph <Ekkasit.ph@kkumail.com>
2026-03-05 19:59:10 +0700

    remove unused model and repository classes for Activity, ActivityStatus, Booking, Category, Community, CommunityAdmin, Location, Notification, Role, User, and UserProfile

--- server/src/main/java/com/thaiglocal/server/model/enums/ReadStatus.java
+++
@@ -1,6 +0,0 @@
-package com.thaiglocal.server.model.enums;
-
-public enum ReadStatus {
-    UNREAD,
-    READ
-}
--- server/src/main/java/com/thaiglocal/server/model/enums/RoleName.java
+++
@@ -1,7 +0,0 @@
-package com.thaiglocal.server.model.enums;
-
-public enum RoleName {
-    GENERAL_USER,
-    COMMUNITY_ADMIN,
-    SYSTEM_ADMIN
-}
